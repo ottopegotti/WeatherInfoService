a330730736ccbed66381da10544da746e19c1e30
ottopegotti <utkukapucu@gmail.com>
2017-11-06 11:06:41 +0200

    .
    .

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('*', function (req, res) {
     const index = path.join(__dirname, 'dist', 'index.html');
     res.sendFile(index);
   });
-const port=process.env.PORT || '8080';
+const port=process.env.PORT || '445';
 app.set('port',port);
 
 
