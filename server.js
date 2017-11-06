7ff32ec9025e03e7042438427e3c40e45d778a85
ottopegotti <utkukapucu@gmail.com>
2017-11-06 02:08:15 +0200

    port is enveloped
    port is enveloped due to a problem occured in heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ session = require('express-session'),
 api = require('./server/api/api'),
 cors = require('cors');
 const path=require ('path');
+const http=require('http');
 
 app = express();
 
@@ -29,6 +30,13 @@ app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,'dist/index.html'));
 });
 
+const port=process.env.PORT || '3001';
+app.set('port',port);
+
+const server=http.createServer(app);
+server.listen(port,() =>console.log("CustMgr Express server listening on port %d",port));
+
+/*
 app.listen(3000, function () {
 console.log("CustMgr Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+});*/
